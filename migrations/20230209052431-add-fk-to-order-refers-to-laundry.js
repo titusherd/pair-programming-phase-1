433c29bc0c7c8b7b5962d97266921a9bce37c894
Titus Herdiawan <titus17001@mail.unpad.ac.id>
2023-02-09 13:52:14 +0700

    setup database

---
+++ migrations/20230209052431-add-fk-to-order-refers-to-laundry.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Orders', "LaundryId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Laundries"
+      }
+    })
+
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn("Orders", "LaundryId", {})
+  }
+};
