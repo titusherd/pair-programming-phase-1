deb0a695638008ce42d4f3a89de71cda3221b0d8
asbon-situmorang <situmorang.asbon@gmail.com>
2023-02-10 00:31:39 +0700

    asbon

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcryptjs = require('bcryptjs')
 const {
   Model
 } = require('sequelize');
@@ -16,16 +17,50 @@ module.exports = (sequelize, DataTypes) => {
       User.belongsToMany(models.Laundry, {
         through: "Order"
       })
-
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Email cannot be empty" },
+        notEmpty: { msg: "Email cannot be empty" },
+        isEmail: { msg: "Not a valid email format" }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Password cannot be empty" },
+        notEmpty: { msg: "Password cannot be empty" },
+        minimumPassLength(value) {
+          if (value.length < 4) {
+            throw new Error('Password atleast 4 characters')
+          }
+        }
+      }
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Role cannot be empty" },
+        notEmpty: { msg: "Role cannot be empty" },
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate(instance, option) {
+        const salt = bcryptjs.genSaltSync(10)
+        const hash = hashsync(instance.password, salt)
+        instance.password = hash
+      }
+    }
   });
+
   return User;
 };
